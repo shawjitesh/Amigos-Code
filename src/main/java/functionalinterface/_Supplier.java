36b24abfaa9395db85b1760cb72a97054df9e29d
Jitesh Shaw <jiteshshaw93@gmail.com>
2022-01-05 17:43:41 +0530

    Supplier

---
+++ src/main/java/functionalinterface/_Supplier.java
@@ -0,0 +1,19 @@
+package functionalinterface;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+        System.out.println(getDbConnectionUrl());
+        System.out.println(getDbConnectionUrlsSupplier.get());
+    }
+
+    static Supplier<List<String>> getDbConnectionUrlsSupplier = () ->
+            List.of("jdbc://localhost:5432/users",
+                    "jdbc://localhost:5432/customers");
+
+    static String getDbConnectionUrl() {
+        return "jdbc://localhost:5432/users";
+    }
+}
